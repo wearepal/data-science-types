444f013fef48dfe3b3a9c7fc3addbca42c22baaf
Oliver Thomas <ot44@sussex.ac.uk>
2020-01-20 13:43:29 +0000

    formatted with black

--- pandas-stubs/core/indexing.pyi
+++ pandas-stubs/core/indexing.pyi
@@ -24,7 +24,8 @@ class _iLocIndexer:
     def __setitem__(
         self,
         idx: Tuple[
-            Union[Series, _np.ndarray[_np.bool_], _np.ndarray[_np._Int], int, Index[int]], Union[str, _np.str_]
+            Union[Series, _np.ndarray[_np.bool_], _np.ndarray[_np._Int], int, Index[int]],
+            Union[str, _np.str_],
         ],
         value: Union[float, _np.ndarray],
     ) -> None: ...
@@ -45,7 +46,8 @@ class _LocIndexer:
     def __setitem__(
         self,
         idx: Tuple[
-            Union[Series, _np.ndarray[_np.bool_], _np.ndarray[_np._Int], int, Index[int]], Union[str, _np.str_]
+            Union[Series, _np.ndarray[_np.bool_], _np.ndarray[_np._Int], int, Index[int]],
+            Union[str, _np.str_],
         ],
         value: Union[float, _np.ndarray],
     ) -> None: ...
