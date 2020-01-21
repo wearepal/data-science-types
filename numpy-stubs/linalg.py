f79da0c5a919491a5f6f3fdf42054d40c8b49e23
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-01-21 15:20:34 +0000

    Format with black

--- numpy-stubs/linalg.py
+++ numpy-stubs/linalg.py
@@ -4,6 +4,6 @@ from numpy import ndarray, _Int, float64, _DType
 
 
 def slogdet(
-    a: ndarray[_DType]
+    a: ndarray[_DType],
 ) -> Union[Tuple[float64, float64], Tuple[ndarray[float64], ndarray[float64]]]:
     ...
