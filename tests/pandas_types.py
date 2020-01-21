cbfecb3e2e9acf799de1c1861416cc4aa53c77e5
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-01-21 13:20:34 +0000

    Format with black

--- tests/pandas_types.py
+++ tests/pandas_types.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import numpy as np
 
 a: pd.DataFrame = pd.DataFrame([[1, 2, 3], [1, 2, 3]], columns=["a", "b", "c"])
-b: pd.Series = a["a"]
+
+
+def test_getitem() -> None:
+    b: pd.Series = a["a"]
+    c: pd.DataFrame = a[["a", "b"]]
+    d: pd.DataFrame = a[("a", "b")]
+    e: pd.DataFrame = a[np.array(["a", "b"])]
+    f: pd.DataFrame = a[pd.Series(["a", "b"])]
+    g: pd.DataFrame = a[pd.Index(["a", "b"])]
