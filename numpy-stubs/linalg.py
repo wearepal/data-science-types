208d5a5061c134da58a0bd0a018581216b5046ce
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-01-21 15:07:46 +0000

    Fix return type of slogdet

--- numpy-stubs/linalg.py
+++ numpy-stubs/linalg.py
@@ -1,7 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 from numpy import ndarray, _Int, float64, _DType
 
 
-def slogdet(x: ndarray[_DType]) -> Tuple[float64, ndarray[float64]]:
+def slogdet(
+    a: ndarray[_DType]
+) -> Union[Tuple[float64, float64], Tuple[ndarray[float64], ndarray[float64]]]:
     ...
