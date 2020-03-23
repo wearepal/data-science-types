771ea46ee8bb0508a09bf8add82a5caa35cc0c3b
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-03-23 16:22:45 +0000

    Add np.squeeze and np.linalg.svd

--- numpy-stubs/linalg.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple, Union
-
-from numpy import ndarray, _Int, float64, _DType
-
-
-def slogdet(
-    a: ndarray[_DType],
-) -> Union[Tuple[float64, float64], Tuple[ndarray[float64], ndarray[float64]]]:
-    ...
