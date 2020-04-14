d38d12c22c09a37ba80f37209de663a319f7d454
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-04-14 10:40:19 +0100

    Fix the input types for matplotlib

--- tests/matplotlib_test.py
+++ tests/matplotlib_test.py
@@ -19,6 +19,7 @@ fig, plot_grid = plt.subplots(squeeze=False)
 fig, plot_grid = plt.subplots(nrows=2, squeeze=False)
 fig, plot_grid = plt.subplots(ncols=2, squeeze=False)
 fig, plot_grid = plt.subplots(nrows=2, ncols=2, squeeze=False)
+
 plt.xticks(ticks=[0, 1], labels=["foo", "bar"])
 plt.yticks(ticks=[0, 1], labels=["foo", "bar"])
 plt.xticks(ticks=np.array([0, 1]), labels=np.array(["foo", "bar"]))
@@ -26,3 +27,9 @@ plt.xticks(ticks=pd.Series([0, 1]), labels=pd.Series(["foo", "bar"]))
 plt.xlim(0, 1)
 plt.ylim(0, 1)
 plt.fill_between((0, 1), y1=(0, 1), y2=(1, 1), where=[True], interpolate=False, data=[])
+
+
+def test_data_types() -> None:
+    x = np.linspace(0, 2)
+    y = np.exp(x)
+    plt.plot(x, y)
