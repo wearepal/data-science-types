a40a179ff1a6ca4de239e8827932180109260a01
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2020-04-14 09:59:13 +0100

    Use ... instead of pass everywhere.

--- numpy-stubs/ma.pyi
+++ numpy-stubs/ma.pyi
@@ -1,8 +1,7 @@
 from numpy import ndarray, _DType, bool_
 from typing import Generic
 
-class MaskedArray(ndarray[_DType]):
-    pass
+class MaskedArray(ndarray[_DType]): ...
 
 def array(data: ndarray[_DType], mask: ndarray[bool_] = ...) -> MaskedArray[_DType]: ...
 def dot(a: MaskedArray[_DType], b: MaskedArray[_DType]) -> MaskedArray[_DType]: ...
