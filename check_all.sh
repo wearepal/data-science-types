2739ba09e33ab8580ee429dc8b3384c994d51665
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-05-22 18:28:00 +0100

    Add generator script to check_all.sh

--- check_all.sh
+++ check_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-black -l 100 -t py36 . && flake8 *-stubs && python -m pytest -vv tests/ && mypy tests
+black -l 100 -t py36 . && flake8 *-stubs && python -m pytest -vv tests/ && python gen_pyi.py && mypy tests
