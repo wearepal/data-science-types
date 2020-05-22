66216e51edda8fa406f14b4a8d8b1baf2cf6b9ad
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-05-22 22:07:43 +0100

    Modify check all script

--- check_all.sh
+++ check_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-black -l 100 -t py36 . && flake8 *-stubs && python -m pytest -vv tests/ && python gen_pyi.py && mypy tests
+python gen_pyi.py && black -l 100 -t py36 . && flake8 *-stubs && python -m pytest -vv tests/ && mypy tests && echo "Check all complete!"
