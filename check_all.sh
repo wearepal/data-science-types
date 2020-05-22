753f3e344a17853ee6412dfb5c5aa3a352b285e9
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-05-22 22:59:32 +0100

    Add more kwargs

--- check_all.sh
+++ check_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python gen_pyi.py && black -l 100 -t py36 . && flake8 *-stubs && python -m pytest -vv tests/ && mypy tests && echo "Check all complete!"
+python gen_pyi.py && black -S -l 100 -t py36 . && flake8 *-stubs && python -m pytest -vv tests/ && mypy tests && echo "Check all complete!"
