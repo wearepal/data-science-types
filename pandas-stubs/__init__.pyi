5213b7a16d39b1dcf156249175f5a83a48f0f74e
smart <nmartinez@dcc.fceia.unr.edu.ar>
2020-06-24 08:54:22 -0300

    Typing read_csv
    Added missing args from https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html

--- pandas-stubs/__init__.pyi
+++ pandas-stubs/__init__.pyi
@@ -10,10 +10,7 @@ from .core.series import Series as Series
 from .core.indexes import Index as Index, MultiIndex as MultiIndex
 
 def concat(
-    dataframes: List[DataFrame],
-    axis: _AxisType = ...,
-    sort: Optional[bool] = ...,
-    ignore_index: bool = ...,
+    dataframes: List[DataFrame], axis: _AxisType = ..., sort: Optional[bool] = ..., ignore_index: bool = ...,
 ) -> DataFrame: ...
 def cut(arr: _np.ndarray, bins: int) -> Tuple[Union[Series, _np.ndarray], _np.ndarray]: ...
 def get_dummies(df: Union[DataFrame, Series], columns: Optional[_ListLike] = ...) -> DataFrame: ...
@@ -21,23 +18,61 @@ def isnull(df: Union[DataFrame, Series]) -> _np.ndarray: ...
 @overload
 def merge(left: DataFrame, right: DataFrame, on: str = ...) -> DataFrame: ...
 @overload
-def merge(
-    left: DataFrame, right: DataFrame, left_on: str, right_on: str, how: str
-) -> DataFrame: ...
+def merge(left: DataFrame, right: DataFrame, left_on: str, right_on: str, how: str) -> DataFrame: ...
 @overload
-def merge(
-    left: DataFrame, right: DataFrame, left_on: List[str], right_on: List[str], how: str
-) -> DataFrame: ...
+def merge(left: DataFrame, right: DataFrame, left_on: List[str], right_on: List[str], how: str) -> DataFrame: ...
 def read_csv(
     filepath_or_buffer: Union[str, Path],
     sep: str = ...,
-    header: Optional[int] = ...,
-    index_col: Optional[Union[str, int, List[str], Tuple[str, ...], Sequence[int], bool]] = ...,
-    delim_whitespace: bool = ...,
+    delimiter: Optional[str] = ...,  # only an alias to sep
+    header: Optional[int, List[int], Literal["infer"]] = ...,
     names: Optional[List[str]] = ...,
-    dtype: Optional[Any] = ...,
-    usecols: Optional[Union[List, Callable]] = ...,
-    skiprows: Optional[Union[int, _ListLike]] = ...,
+    index_col: Optional[Union[str, int, List[str], Tuple[str, ...], Sequence[int], bool]] = ...,
+    usecols: Optional[Union[List[str], List[int], Callable]] = ...,
+    squeeze: bool = ...,
+    prefix: Optional[str] = ...,
+    mangle_dupe_cols: bool = ...,
+    dtype: Optional[Union[Type, Dict[str, Type]]] = ...,
+    engine: Optional[Union[Literal["c"], Literal["python"]]] = ...,
+    converters: Dict[Union[str, int], Callable] = ...,
+    true_values: Optional[List] = ...,
+    false_values: Optional[List] = ...,
+    skipinitialspace: bool = ...,
+    skiprows: Optional[Union[int, _ListLike, Callable]] = ...,
+    skipfooter: int = ...,
+    nrows: Optional[int] = ...,
+    na_values: Optional[Union[str, List[str]]] = ...,
+    keep_default_na: bool = ...,
+    na_filter: bool = ...,
+    verbose: bool = ...,
+    skip_blank_line: bool = ...,
+    parse_dates: Union[bool, List[int], List[str], List[List[int]], Dict[str, List[int]]] = ...,
+    infer_datetime_format: bool = ...,
+    keep_date_col: bool = ...,
+    date_parser: Optional[Callable] = ...,
+    dayfirst: bool = ...,
+    cache_dates: bool = ...,
+    iterator: bool = ...,
+    chunksize: Optional[int] = ...,
+    compression: Optional[
+        Union[Literal["infer"], Literal["gzip"], Literal["bz3"], Literal["zip"], Literal["xz"]]
+    ] = ...,
+    thousands: Optional[str] = ...,
+    decimal: Optional[str] = ...,
+    lineterminator: Optional[str] = ...,
+    quotechar: Optional[str] = ...,
+    quoting: Optional[Literal[0], Literal[1], Literal[2], Literal[3]] = ...,
+    doublequote: bool = ...,
+    escapechar: Optional[str] = ...,
+    comment: Optional[str] = ...,
+    encoding: Optional[str] = ...,
+    dialect: Any = ...,  # TODO str or csv.Dialect Optional
+    error_bad_lines: bool = ...,
+    warn_bad_lines: bool = ...,
+    delim_whitespace: bool = ...,
+    low_memory: bool = ...,
+    memory_map: bool = ...,
+    float_precision: Optional[str] = ...,
 ) -> DataFrame: ...
 def read_sql(
     sql: Union[str, Any],
@@ -52,7 +87,7 @@ def read_sql(
 def read_feather(p: Union[Path, IO]) -> DataFrame: ...
 def to_numeric(
     arg: Union[int, float, List, Tuple, _np.ndarray, Series],
-    errors: Literal['ignore', 'raise', 'coerce'] = ...,
-    downcast: Literal['integer', 'signed', 'unsigned', 'float'] = ...,
+    errors: Literal["ignore", "raise", "coerce"] = ...,
+    downcast: Literal["integer", "signed", "unsigned", "float"] = ...,
 ) -> Union[Series, _np.ndarray]: ...
 def unique(values: Series) -> _np.ndarray: ...
