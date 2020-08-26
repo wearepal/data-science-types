73ccfdb67c8684ee4050bb3321783c333892955a
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2020-08-26 17:38:24 +0100

    minor updates. Probably wrong stubs

--- pandas-stubs/core/series.pyi
+++ pandas-stubs/core/series.pyi
@@ -47,7 +47,7 @@ class Series(Generic[_DType]):
     def __init__(
         self,
         data: Optional[
-            Union[_ListLike[_DType], Series[_DType], Dict[int, _DType], Dict[_str, _DType]]
+            Union[_ListLike[_DType], Series[_DType], Dict[int, _DType], Dict[_str, _DType], int]
         ],
         index: Union[_str, int, Series, Index, range] = ...,
     ): ...
