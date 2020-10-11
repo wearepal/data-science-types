c1a0fe00d9093e5c3069d65af9559c6e65d94c87
Thomas M <thomas5max@gmail.com>
2020-10-11 14:16:12 +0100

    Bump version to 0.2.19

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ gen_pyi()
 
 setup(
     name="data-science-types",
-    version="0.2.18",
+    version="0.2.19",
     author="PAL",
     description="Type stubs for Python machine learning libraries",
     license="Apache License 2.0",
